2feb323d911ff992b35bc344654b47d7b4687583
Alexa Amundson <alexa@blackroad.io>
2026-03-29 01:42:57 -0500

    init: work.blackroad.io worker source
    RoadChain-SHA2048: 8edfb8d8fdeae3fc
    RoadChain-Identity: alexa@sovereign
    RoadChain-Full: 8edfb8d8fdeae3fcfe9e9185dddd581b608184d3a6a2bbef32ff1b3fbca2bb0de80076eaf68f1a11f3e00ed5b2d008832ca0064a07c4c3fe44ad7f2d59345128ada9d0aeb2c96c58291fa242bb9a8009e191c1e83dbf927a8c4557872d5465e4ea8244414ba8a7134930ee39c8185f5d5ab09e56444d633965431bbb204f3d401784483dac6098aeb80766fe8eb3f0f27d3ba673f4c3dcaf9bfd916cd75c93076aeb98c75f50216034a12daa204819af22d0d61ea15e14df38cb0f64292735967daf42523d71047ea8c5451f5dc6f82d78cbd798a429dcf8fa25d0e12f9cb37901943fe0cebec50d071f7a360b09c335ebcaff660db0c0faeefbc6ff27f0d67c

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+// index.js
+var index_default = { async fetch() {
+  return Response.redirect("https://tutor.blackroad.io", 302);
+} };
+export {
+  index_default as default
+};
+//# sourceMappingURL=index.js.map
+
